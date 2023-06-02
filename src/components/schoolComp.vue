0bb6215e05deb566e9f4e5d24e4f98cb6f84f872
BackThePortal <backtheportal@gmail.com>
2023-06-02 18:50:27 +0200

    Added rounding in stars

--- src/components/schoolComp.vue
+++ src/components/schoolComp.vue
@@ -40,7 +40,9 @@
 						<ion-text style="">
 							{{
 								school.rates
-									? school.rates?.total / school.rates?.count + ' / 5'
+									? (school?.rates?.count
+											? (school?.rates?.total / school?.rates?.count).toFixed(2)
+											: '?') + ' / 5'
 									: 'Sense ressenyes'
 							}}
 						</ion-text>
