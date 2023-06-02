7622287d65c26e68d6933352dac33c3fa1afd774
BackThePortal <backtheportal@gmail.com>
2023-06-02 18:50:07 +0200

    Added sort in search

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,6 +1,11 @@
 <template>
 	<TabLayout>
 		<ion-searchbar v-model="query" placeholder="Cerca per nom"></ion-searchbar>
+		<ion-select :label="'Ordena per'" v-model="order">
+			<ion-select-option v-for="option in orderOptions" :value="option.id">{{
+				option.show
+			}}</ion-select-option>
+		</ion-select>
 
 		<SchoolComp v-for="school in results" :school="school" :geo="geoData" />
 	</TabLayout>
@@ -10,7 +15,7 @@
 import TabLayout from '@/layout/tabLayout.vue';
 
 import { Geolocation } from '@capacitor/geolocation';
-import { schoolData } from '@/data';
+import { School, schoolData } from '@/data';
 import SchoolComp from '@/components/schoolComp.vue';
 import { IonSearchbar } from '@ionic/vue';
 
@@ -42,8 +47,36 @@ const results = computed(() => {
 
 					return item;
 				})
+				.sort(orderOptions.find((item) => item.id === order.value)?.function)
 		: [];
 });
+
+const orderOptions: {
+	id: string;
+	show: string;
+	function: (a: School, b: School) => any;
+}[] = [
+	{
+		id: 'distance',
+		show: 'Distància',
+		function: (a, b) => a.distance - b.distance,
+	},
+	{
+		id: 'name',
+		show: 'Nom',
+		function: (a, b) => a.name.localeCompare(b.name),
+	},
+	/*
+	{
+		id: 'score',
+		show: 'Puntuació',
+		function: (a, b) => (Number.isNaN(a.rates?.total / a.rates?.count) ? 0 a.rates?.total / a.rates?.count) - (Number.isNaN(b.rates?.total / b.rates?.count) ? 0 b.rates?.total / b.rates?.count)
+	}
+
+		 */
+];
+
+const order = ref(orderOptions[0].id);
 
 Geolocation.getCurrentPosition().then((value) => {
 	geoData.value = [
